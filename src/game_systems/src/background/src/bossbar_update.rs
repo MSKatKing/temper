2ad60e637f25972fafb43c5c06fd936e2bccd423
ReCore <kjshore2004@gmail.com>
2026-05-01 23:45:21 +0930

    Added encoder for the packet

--- src/game_systems/src/background/src/bossbar_update.rs
+++ src/game_systems/src/background/src/bossbar_update.rs
@@ -1,10 +1,10 @@
 use bevy_ecs::prelude::Query;
-use tracing::warn;
 use temper_components::player::bossbar_sender::BossbarSender;
 use temper_net_runtime::connection::StreamWriter;
+use temper_protocol::outgoing::boss_event::BossbarPacket;
 use temper_resources::bossbar::{BossBarResource, UpdateBBKind};
+use tracing::warn;
 use uuid::Uuid;
-use temper_protocol::outgoing::bossbar_packet::BossbarPacket;
 
 pub fn update_bossbars(
     player_query: Query<(&StreamWriter, &BossbarSender)>,
@@ -48,16 +48,29 @@ pub fn update_bossbars(
     for (writer, _bossbar_sender) in player_query.iter() {
         for (uuid, update_kind) in &updated {
             let packet = match update_kind {
-                UpdateBBKind::Add { data } => {
-                    BossbarPacket::add_bossbar(uuid.as_u128(), data.title.clone(), data.health, data.color.discriminant().into(), data.dividers.discriminant().into(), data.flags.get())
-                }
+                UpdateBBKind::Add { data } => BossbarPacket::add_bossbar(
+                    uuid.as_u128(),
+                    data.title.clone(),
+                    data.health,
+                    data.color.discriminant().into(),
+                    data.dividers.discriminant().into(),
+                    data.flags.get(),
+                ),
                 UpdateBBKind::Remove => BossbarPacket::remove_bossbar(uuid.as_u128()),
-                UpdateBBKind::UpdateHealth { new_health } => BossbarPacket::update_health(uuid.as_u128(), *new_health),
-                UpdateBBKind::UpdateTitle { title } => BossbarPacket::update_title(uuid.as_u128(), title.clone()),
-                UpdateBBKind::UpdateStyle { color, dividers } => {
-                    BossbarPacket::update_style(uuid.as_u128(), color.discriminant().into(), dividers.discriminant().into())
+                UpdateBBKind::UpdateHealth { new_health } => {
+                    BossbarPacket::update_health(uuid.as_u128(), *new_health)
                 }
-                UpdateBBKind::UpdateFlags { flags } => BossbarPacket::update_flags(uuid.as_u128(), flags.get()),
+                UpdateBBKind::UpdateTitle { title } => {
+                    BossbarPacket::update_title(uuid.as_u128(), title.clone())
+                }
+                UpdateBBKind::UpdateStyle { color, dividers } => BossbarPacket::update_style(
+                    uuid.as_u128(),
+                    color.discriminant().into(),
+                    dividers.discriminant().into(),
+                ),
+                UpdateBBKind::UpdateFlags { flags } => {
+                    BossbarPacket::update_flags(uuid.as_u128(), flags.get())
+                }
             };
             writer.send_packet_ref(&packet).unwrap_or_else(|_| {
                 warn!("Failed to send Bossbar Packet to player");
