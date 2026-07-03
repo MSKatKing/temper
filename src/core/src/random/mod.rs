294a89a4a27ed2ed56a6bda1d199483925df68ea
MSKatKing <mskatking08@gmail.com>
2026-07-03 14:50:10 -0500

    Implement XoroshiroRandomSource

--- src/core/src/random/mod.rs
+++ src/core/src/random/mod.rs
@@ -1,6 +1,10 @@
+mod xoroshiro;
+
 use std::borrow::Borrow;
 use std::range::RangeInclusive;
 use crate::pos::BlockPos;
+
+pub use xoroshiro::{XoroshiroRandomSource, XoroshiroPositionalRandom};
 
 /// Supplies various random functions for structs. Based on `net.minecraft.util.RandomSource`.
 pub trait RandomSource {
@@ -18,6 +22,8 @@ pub trait RandomSource {
     fn next_bool(&mut self) -> bool;
     fn next_f32(&mut self) -> f32;
     fn next_f64(&mut self) -> f64;
+
+    fn consume_count(&mut self, count: usize);
 }
 
 /// Provides a way to spawn various RandomSources from input seeds.
@@ -29,4 +35,57 @@ pub trait PositionalRandom<R: RandomSource> {
     fn spawn_at_pos(&self, pos: BlockPos) -> R {
         self.spawn_at(pos.pos.x, pos.pos.y, pos.pos.z)
     }
+}
+
+/// Converts the low and high bits (u64s) to a single u128
+#[inline]
+fn u64_to_u128(low: u64, high: u64) -> u128 {
+    (high as u128) << 64 | low as u128
+}
+
+/// Returns the lowest 64 bits of a u128
+#[inline]
+fn u128_low(seed: u128) -> u64 {
+    seed as u64
+}
+
+/// Returns the highest 64 bits of a u128
+#[inline]
+fn u128_high(seed: u128) -> u64 {
+    (seed >> 64) as u64
+}
+
+fn mix_stafford13(val: u64) -> u64 {
+    let val = (val ^ val >> 30).wrapping_mul(0xbf58476d1ce4e5b9);
+    let val = (val ^ val >> 27).wrapping_mul(0x94d049bb133111eb);
+    val * val >> 31
+}
+
+#[inline]
+#[allow(dead_code)]
+fn upgrade_seed_unmixed(seed: u64) -> u128 {
+    let low = seed ^ 0x6a09e667f3bcc909;
+    let high = low.wrapping_add(0x9e3779b97f4a7c15);
+    u64_to_u128(low, high)
+}
+
+#[inline]
+fn upgrade_seed_mixed(seed: u64) -> u128 {
+    let low = seed ^ 0x6a09e667f3bcc909;
+    let high = low.wrapping_add(0x9e3779b97f4a7c15);
+    u64_to_u128(mix_stafford13(low), mix_stafford13(high))
+}
+
+#[inline]
+fn seed_from_hash(hash: &str) -> u128 {
+    let hash_bytes = md5::compute(hash).0;
+    let [low, high]: [[u8; 8]; 2] = unsafe { std::mem::transmute(hash_bytes) };
+    u64_to_u128(u64::from_be_bytes(low), u64::from_be_bytes(high))
+}
+
+#[inline]
+fn seed_from_pos(x: i32, y: i32, z: i32) -> u64 {
+    let seed = x.wrapping_mul(3129871) as u64 ^ z.wrapping_mul(116129781) as u64 ^ y as u64;
+    let seed = seed.wrapping_mul(seed).wrapping_mul(42317861).wrapping_add(seed.wrapping_mul(11));
+    seed >> 16
 }
