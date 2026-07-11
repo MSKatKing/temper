910902dc1400601a688db12d33f946e7f9dc8e2b
Kumpelinus <linus@kump.dev>
2026-07-11 02:21:23 +0200

    fix op command message

--- src/default_commands/src/op.rs
+++ src/default_commands/src/op.rs
@@ -3,6 +3,7 @@ use temper_command_infra::args::EntitiesArg;
 use temper_command_infra::{CommandHandler, CommandResult, CommandSource};
 use temper_components::entity_identity::Identity;
 use temper_components::player::player_marker::PlayerMarker;
+use temper_core::mq;
 use temper_macros::Command;
 use temper_permissions::Access::Allow;
 use temper_permissions::Permissions;
@@ -23,7 +24,7 @@ impl CommandHandler for OpCommand {
 
     fn handle(
         self,
-        source: CommandSource,
+        _source: CommandSource,
         params: &mut Self::SystemParam<'_, '_>,
     ) -> CommandResult {
         let (entities, permissions) = params;
@@ -31,7 +32,11 @@ impl CommandHandler for OpCommand {
         for entity in self.target.resolve(entities.iter()) {
             if let Ok(mut player_permission) = permissions.get_mut(entity) {
                 player_permission.set_permission(Permissions::ALL, Allow);
-                source.send_message(TextComponent::from("You have been opped".to_string()));
+                mq::queue(
+                    TextComponent::from("You have been opped".to_string()),
+                    false,
+                    entity,
+                );
             }
         }
 
