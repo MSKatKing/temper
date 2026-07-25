e81bbe91f347e7dfaf064163fb010d49be14fa55
MSKatKing <mskatking08@gmail.com>
2026-07-24 22:26:26 -0500

    Implement and test NormalNoise

--- src/world/noise/src/lib.rs
+++ src/world/noise/src/lib.rs
@@ -1,8 +1,11 @@
 mod improved;
 mod perlin;
+mod normal;
 
 use bevy_math::DVec3;
 pub use improved::ImprovedNoise;
+pub use perlin::PerlinNoise;
+pub use normal::NormalNoise;
 
 const GRADIENT: [DVec3; 16] = [
     DVec3::new(1.0, 1.0, 0.0),
