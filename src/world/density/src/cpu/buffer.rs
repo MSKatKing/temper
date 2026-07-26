d4540c69c19ba524c445c405e89a4e721277926f
MSKatKing <mskatking08@gmail.com>
2026-07-26 14:46:37 -0500

    Proof of concept for new density evaluation idea

---
+++ src/world/density/src/cpu/buffer.rs
@@ -0,0 +1,46 @@
+#[derive(Copy, Clone, Hash, Eq, PartialEq)]
+pub enum BufferType {
+    Out,
+    Full,
+    Flat,
+    FlatCell,
+    Interpolated,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct BufferId {
+    pub ty: BufferType,
+    pub id: u8,
+}
+
+pub struct Buffer {
+    pub ty: BufferType,
+    pub data: Box<[f32]>,
+}
+
+impl BufferType {
+    pub fn size(&self) -> usize {
+        match self {
+            BufferType::Out | BufferType::Full => 16 * 16 * 320,
+            BufferType::Flat => 16 * 16,
+            BufferType::FlatCell => 4 * 4,
+            BufferType::Interpolated => 8 * (4 * 4 * (320 / 4))
+        }
+    }
+}
+
+impl BufferId {
+    pub const OUT: BufferId = BufferId { ty: BufferType::Out, id: 0 };
+
+    pub fn flat(id: u8) -> BufferId {
+        BufferId { ty: BufferType::Flat, id }
+    }
+
+    pub fn flat_cell(id: u8) -> BufferId {
+        BufferId { ty: BufferType::FlatCell, id }
+    }
+
+    pub fn interpolated(id: u8) -> BufferId {
+        BufferId { ty: BufferType::Interpolated, id }
+    }
+}
