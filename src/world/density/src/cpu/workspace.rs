67021a6757af03b87de3bb3c46b19fa7368a2e76
MSKatKing <mskatking08@gmail.com>
2026-07-27 01:20:35 -0500

    Restructure and add a few more math operations

---
+++ src/world/density/src/cpu/workspace.rs
@@ -0,0 +1,91 @@
+use crate::cpu::buffer::{Buffer, BufferId, BufferType};
+use crate::cpu::operation::Operation;
+use crate::cpu::runtime::execute_function;
+
+pub struct Workspace<'func> {
+    pub out: Buffer,
+    pub full: Vec<Buffer>,
+    pub flat: Vec<Buffer>,
+    pub flat_cell: Vec<Buffer>,
+    pub interpolated: Vec<Buffer>,
+    pub operations: &'func [Operation],
+}
+
+impl Workspace<'_> {
+    pub fn get_buffer(&self, id: BufferId) -> Option<&Buffer> {
+        match id.ty {
+            BufferType::Out => Some(&self.out),
+            BufferType::Full => self.full.get(id.id as usize),
+            BufferType::Flat => self.flat.get(id.id as usize),
+            BufferType::FlatCell => self.flat_cell.get(id.id as usize),
+            BufferType::Interpolated => self.interpolated.get(id.id as usize),
+        }
+    }
+
+    pub fn get_buffer_mut(&mut self, id: BufferId) -> Option<&mut Buffer> {
+        match id.ty {
+            BufferType::Out => Some(&mut self.out),
+            BufferType::Full => self.full.get_mut(id.id as usize),
+            BufferType::Flat => self.flat.get_mut(id.id as usize),
+            BufferType::FlatCell => self.flat_cell.get_mut(id.id as usize),
+            BufferType::Interpolated => self.interpolated.get_mut(id.id as usize),
+        }
+    }
+
+    pub fn get_dst_src(&mut self, dst: BufferId, src: BufferId) -> Option<(&mut Buffer, &Buffer)> {
+        if dst == src {
+            return None;
+        }
+
+        match (dst.ty, src.ty) {
+            (BufferType::Out, BufferType::Out) => unreachable!(),
+            (BufferType::Full, BufferType::Full) => split_two(&mut self.full, dst.id as _, src.id as _),
+            (BufferType::Flat, BufferType::Flat) => split_two(&mut self.flat, dst.id as _, src.id as _),
+            (BufferType::FlatCell, BufferType::FlatCell) => split_two(&mut self.flat, dst.id as _, src.id as _),
+            (BufferType::Interpolated, BufferType::Interpolated) => split_two(&mut self.interpolated, dst.id as _, src.id as _),
+
+            (BufferType::Out, BufferType::Flat) => Some((&mut self.out, self.flat.get(src.id as usize)?)),
+            (BufferType::Out, BufferType::FlatCell) => Some((&mut self.out, self.flat_cell.get(src.id as usize)?)),
+            (BufferType::Out, BufferType::Full) => Some((&mut self.out, self.full.get(dst.id as usize)?)),
+            (BufferType::Out, BufferType::Interpolated) => Some((&mut self.out, self.interpolated.get(dst.id as usize)?)),
+
+            (BufferType::Full, BufferType::Out) => Some((self.full.get_mut(dst.id as usize)?, &self.out)),
+            (BufferType::Full, BufferType::Flat) => Some((self.full.get_mut(dst.id as usize)?, self.flat.get(src.id as usize)?)),
+            (BufferType::Full, BufferType::FlatCell) => Some((self.full.get_mut(dst.id as usize)?, self.flat_cell.get(src.id as usize)?)),
+            (BufferType::Full, BufferType::Interpolated) => Some((self.full.get_mut(dst.id as usize)?, self.interpolated.get(src.id as usize)?)),
+
+            (BufferType::Flat, BufferType::Out) => Some((self.flat.get_mut(dst.id as usize)?, &self.out)),
+            (BufferType::Flat, BufferType::Full) => Some((self.flat.get_mut(dst.id as usize)?, self.full.get(src.id as usize)?)),
+            (BufferType::Flat, BufferType::FlatCell) => Some((self.flat.get_mut(dst.id as usize)?, self.flat_cell.get(src.id as usize)?)),
+            (BufferType::Flat, BufferType::Interpolated) => Some((self.flat.get_mut(dst.id as usize)?, self.interpolated.get(src.id as usize)?)),
+
+            (BufferType::FlatCell, BufferType::Out) => Some((self.flat_cell.get_mut(dst.id as usize)?, &self.out)),
+            (BufferType::FlatCell, BufferType::Full) => Some((self.flat_cell.get_mut(dst.id as usize)?, self.full.get(src.id as usize)?)),
+            (BufferType::FlatCell, BufferType::Flat) => Some((self.flat_cell.get_mut(dst.id as usize)?, self.flat.get(src.id as usize)?)),
+            (BufferType::FlatCell, BufferType::Interpolated) => Some((self.flat_cell.get_mut(dst.id as usize)?, self.interpolated.get(src.id as usize)?)),
+
+            (BufferType::Interpolated, BufferType::Out) => Some((self.interpolated.get_mut(dst.id as usize)?, &self.out)),
+            (BufferType::Interpolated, BufferType::Full) => Some((self.interpolated.get_mut(dst.id as usize)?, self.full.get(src.id as usize)?)),
+            (BufferType::Interpolated, BufferType::Flat) => Some((self.interpolated.get_mut(dst.id as usize)?, self.flat.get(src.id as usize)?)),
+            (BufferType::Interpolated, BufferType::FlatCell) => Some((self.interpolated.get_mut(dst.id as usize)?, self.flat_cell.get(src.id as usize)?)),
+        }
+    }
+    
+    pub fn execute(&mut self) -> Option<()> {
+        execute_function(self)
+    }
+}
+
+fn split_two<T>(slice: &mut [T], a: usize, b: usize) -> Option<(&mut T, &T)> {
+    if a == b {
+        return None;
+    }
+
+    if a < b {
+        let (left, right) = slice.split_at_mut(b);
+        Some((&mut left[a], &right[0]))
+    } else {
+        let (left, right) = slice.split_at_mut(a);
+        Some((&mut right[0], &mut left[0]))
+    }
+}
