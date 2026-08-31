41f7330dacc6dea03db2a924bf4cb6f9d50cdd53
MSKatKing <mskatking08@gmail.com>
2026-08-31 13:15:13 -0400

    Reimplement compiler for Add and restructure noise value source

--- src/world/density/src/cpu/operation.rs
+++ src/world/density/src/cpu/operation.rs
@@ -77,11 +77,6 @@ pub enum PowAmount {
     Reciprocal,
 }
 
-#[derive(Clone, Eq, PartialEq, Debug)]
-pub enum NoiseAccessType {
-    Shift,
-}
-
 impl PowAmount {
     pub fn as_i32(&self) -> i32 {
         match self {
