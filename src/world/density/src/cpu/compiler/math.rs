1594fd87b31dde27ee63d49eb95ffaa77c1fc85e
MSKatKing <mskatking08@gmail.com>
2026-09-03 20:41:50 -0400

    Convert basic math functions over to new system

--- src/world/density/src/cpu/compiler/math.rs
+++ src/world/density/src/cpu/compiler/math.rs
@@ -1,125 +1,149 @@
 use crate::DensityFunctionArgument;
-use crate::cpu::compiler::visitor::{
-    AddBufferVisitor, AddConstantVisitor, AddNoiseVisitor, FillConstantVisitor, FillNoiseVisitor,
-    MulBufferVisitor, MulConstantVisitor, MulNoiseVisitor,
-};
+use crate::cpu::compiler::visitor::{AddBufferVisitor, AddConstantVisitor, AddNoiseVisitor, DivBufferVisitor, DivConstantVisitor, DivNoiseVisitor, FillConstantVisitor, FillNoiseVisitor, MaxBufferVisitor, MaxConstantVisitor, MaxNoiseVisitor, MinBufferVisitor, MinConstantVisitor, MinNoiseVisitor, MulBufferVisitor, MulConstantVisitor, MulNoiseVisitor, SubBufferVisitor, SubConstantVisitor, SubNoiseVisitor};
 use crate::cpu::compiler::{AnyBufferId, Compiler, ReturnValue, compile_arg};
 use temper_core::random::{PositionalRandom, RandomSource};
 
-pub fn compile_add<R: RandomSource, P: PositionalRandom<R>>(
-    compiler: &mut Compiler,
-    rand: &mut P,
-    left: &DensityFunctionArgument,
-    right: &DensityFunctionArgument,
-    parent_buffer: AnyBufferId,
-) -> AnyBufferId {
-    let left_source = compile_arg(compiler, rand, left, parent_buffer);
+macro_rules! compile_commutative {
+    ($name:ident, $buffer:ty, $constant:ty, $noise:ty) => {
+        pub fn $name<R: RandomSource, P: PositionalRandom<R>>(
+            compiler: &mut Compiler,
+            rand: &mut P,
+            left: &DensityFunctionArgument,
+            right: &DensityFunctionArgument,
+            parent_buffer: AnyBufferId,
+        ) -> AnyBufferId {
+            let left_source = compile_arg(compiler, rand, left, parent_buffer);
 
-    let right_buffer = if let ReturnValue::Buffer(left) = &left_source
-        && *left == parent_buffer
-    {
-        compiler.alloc_buffer(parent_buffer)
-    } else {
-        parent_buffer
+            let right_buffer = if let ReturnValue::Buffer(left) = &left_source
+                && *left == parent_buffer
+            {
+                compiler.alloc_buffer(parent_buffer)
+            } else {
+                parent_buffer
+            };
+            let right_source = compile_arg(compiler, rand, right, right_buffer);
+
+            if let ReturnValue::Buffer(left) = &left_source
+                && *left != parent_buffer
+            {
+                compiler.free_buffer(*left);
+            }
+
+            if let ReturnValue::Buffer(right) = &right_source
+                && *right != parent_buffer
+            {
+                compiler.free_buffer(*right);
+            }
+
+            match (left_source, right_source) {
+                (ReturnValue::Constant(left), ReturnValue::Constant(right)) => {
+                    compiler.push_visitor(FillConstantVisitor::new(parent_buffer, left + right))
+                }
+                (ReturnValue::Constant(val), ReturnValue::Noise(noise))
+                | (ReturnValue::Noise(noise), ReturnValue::Constant(val)) => {
+                    let noise = compiler.push_visitor(FillNoiseVisitor::new(parent_buffer, noise));
+
+                    compiler.push_visitor(<$constant>::new(noise, val))
+                }
+                (ReturnValue::Noise(noise_a), ReturnValue::Noise(noise_b)) => {
+                    let noise_a = compiler.push_visitor(FillNoiseVisitor::new(parent_buffer, noise_a));
+
+                    compiler.push_visitor(<$noise>::new(noise_a, noise_b))
+                }
+                (ReturnValue::Noise(noise), ReturnValue::Buffer(buffer))
+                | (ReturnValue::Buffer(buffer), ReturnValue::Noise(noise)) => {
+                    compiler.push_visitor(<$noise>::new(buffer, noise))
+                }
+                (ReturnValue::Constant(val), ReturnValue::Buffer(buffer))
+                | (ReturnValue::Buffer(buffer), ReturnValue::Constant(val)) => {
+                    compiler.push_visitor(<$constant>::new(buffer, val))
+                }
+                (ReturnValue::Buffer(left), ReturnValue::Buffer(right)) => {
+                    compiler.push_visitor(<$buffer>::new(left, right))
+                }
+            }
+        }
     };
-    let right_source = compile_arg(compiler, rand, right, right_buffer);
-
-    if let ReturnValue::Buffer(left) = &left_source
-        && *left != parent_buffer
-    {
-        compiler.free_buffer(*left);
-    }
-
-    if let ReturnValue::Buffer(right) = &right_source
-        && *right != parent_buffer
-    {
-        compiler.free_buffer(*right);
-    }
-
-    match (left_source, right_source) {
-        (ReturnValue::Constant(left), ReturnValue::Constant(right)) => {
-            compiler.push_visitor(FillConstantVisitor::new(parent_buffer, left + right))
-        }
-        (ReturnValue::Constant(val), ReturnValue::Noise(noise))
-        | (ReturnValue::Noise(noise), ReturnValue::Constant(val)) => {
-            let noise = compiler.push_visitor(FillNoiseVisitor::new(parent_buffer, noise));
-
-            compiler.push_visitor(AddConstantVisitor::new(noise, val))
-        }
-        (ReturnValue::Noise(noise_a), ReturnValue::Noise(noise_b)) => {
-            let noise_a = compiler.push_visitor(FillNoiseVisitor::new(parent_buffer, noise_a));
-
-            compiler.push_visitor(AddNoiseVisitor::new(noise_a, noise_b))
-        }
-        (ReturnValue::Noise(noise), ReturnValue::Buffer(buffer))
-        | (ReturnValue::Buffer(buffer), ReturnValue::Noise(noise)) => {
-            compiler.push_visitor(AddNoiseVisitor::new(buffer, noise))
-        }
-        (ReturnValue::Constant(val), ReturnValue::Buffer(buffer))
-        | (ReturnValue::Buffer(buffer), ReturnValue::Constant(val)) => {
-            compiler.push_visitor(AddConstantVisitor::new(buffer, val))
-        }
-        (ReturnValue::Buffer(left), ReturnValue::Buffer(right)) => {
-            compiler.push_visitor(AddBufferVisitor::new(left, right))
-        }
-    }
 }
 
-pub fn compile_mul<R: RandomSource, P: PositionalRandom<R>>(
-    compiler: &mut Compiler,
-    rand: &mut P,
-    left: &DensityFunctionArgument,
-    right: &DensityFunctionArgument,
-    parent_buffer: AnyBufferId,
-) -> AnyBufferId {
-    let left_source = compile_arg(compiler, rand, left, parent_buffer);
+macro_rules! compile_non_commutative {
+    ($name:ident, $buffer:ty, $constant:ty, $noise:ty) => {
+        pub fn $name<R: RandomSource, P: PositionalRandom<R>>(
+            compiler: &mut Compiler,
+            rand: &mut P,
+            left: &DensityFunctionArgument,
+            right: &DensityFunctionArgument,
+            parent_buffer: AnyBufferId,
+        ) -> AnyBufferId {
+            let left_source = compile_arg(compiler, rand, left, parent_buffer);
 
-    let right_buffer = if let ReturnValue::Buffer(left) = &left_source
-        && *left == parent_buffer
-    {
-        compiler.alloc_buffer(parent_buffer)
-    } else {
-        parent_buffer
+            let right_buffer = if let ReturnValue::Buffer(left) = &left_source
+                && *left == parent_buffer
+            {
+                compiler.alloc_buffer(parent_buffer)
+            } else {
+                parent_buffer
+            };
+            let right_source = compile_arg(compiler, rand, right, right_buffer);
+
+            if let ReturnValue::Buffer(left) = &left_source
+                && *left != parent_buffer
+            {
+                compiler.free_buffer(*left);
+            }
+
+            if let ReturnValue::Buffer(right) = &right_source
+                && *right != parent_buffer
+            {
+                compiler.free_buffer(*right);
+            }
+
+            match (left_source, right_source) {
+                (ReturnValue::Constant(left), ReturnValue::Constant(right)) => {
+                    compiler.push_visitor(FillConstantVisitor::new(parent_buffer, left / right))
+                }
+                (ReturnValue::Constant(val), ReturnValue::Noise(noise)) => {
+                    let val = compiler.push_visitor(FillConstantVisitor::new(parent_buffer, val));
+
+                    compiler.push_visitor(<$noise>::new(val, noise))
+                }
+                (ReturnValue::Noise(noise), ReturnValue::Constant(val)) => {
+                    let noise = compiler.push_visitor(FillNoiseVisitor::new(parent_buffer, noise));
+
+                    compiler.push_visitor(<$constant>::new(noise, val))
+                }
+                (ReturnValue::Noise(noise_a), ReturnValue::Noise(noise_b)) => {
+                    let noise_a = compiler.push_visitor(FillNoiseVisitor::new(parent_buffer, noise_a));
+
+                    compiler.push_visitor(<$noise>::new(noise_a, noise_b))
+                }
+                (ReturnValue::Noise(noise), ReturnValue::Buffer(buffer)) => {
+                    let noise = compiler.push_visitor(<$noise>::new(parent_buffer, noise));
+
+                    compiler.push_visitor(<$buffer>::new(noise, buffer))
+                }
+                (ReturnValue::Buffer(buffer), ReturnValue::Noise(noise)) => {
+                    compiler.push_visitor(<$noise>::new(buffer, noise))
+                }
+                (ReturnValue::Constant(val), ReturnValue::Buffer(buffer)) => {
+                    let val = compiler.push_visitor(<$constant>::new(parent_buffer, val));
+
+                    compiler.push_visitor(<$buffer>::new(val, buffer))
+                }
+                (ReturnValue::Buffer(buffer), ReturnValue::Constant(val)) => {
+                    compiler.push_visitor(<$constant>::new(buffer, val))
+                }
+                (ReturnValue::Buffer(left), ReturnValue::Buffer(right)) => {
+                    compiler.push_visitor(<$buffer>::new(left, right))
+                }
+            }
+        }
     };
-    let right_source = compile_arg(compiler, rand, right, right_buffer);
-
-    if let ReturnValue::Buffer(left) = &left_source
-        && *left != parent_buffer
-    {
-        compiler.free_buffer(*left);
-    }
-
-    if let ReturnValue::Buffer(right) = &right_source
-        && *right != parent_buffer
-    {
-        compiler.free_buffer(*right);
-    }
-
-    match (left_source, right_source) {
-        (ReturnValue::Constant(left), ReturnValue::Constant(right)) => {
-            compiler.push_visitor(FillConstantVisitor::new(parent_buffer, left * right))
-        }
-        (ReturnValue::Constant(val), ReturnValue::Noise(noise))
-        | (ReturnValue::Noise(noise), ReturnValue::Constant(val)) => {
-            let noise = compiler.push_visitor(FillNoiseVisitor::new(parent_buffer, noise));
-
-            compiler.push_visitor(MulConstantVisitor::new(noise, val))
-        }
-        (ReturnValue::Noise(noise_a), ReturnValue::Noise(noise_b)) => {
-            let noise_a = compiler.push_visitor(FillNoiseVisitor::new(parent_buffer, noise_a));
-
-            compiler.push_visitor(MulNoiseVisitor::new(noise_a, noise_b))
-        }
-        (ReturnValue::Noise(noise), ReturnValue::Buffer(buffer))
-        | (ReturnValue::Buffer(buffer), ReturnValue::Noise(noise)) => {
-            compiler.push_visitor(MulNoiseVisitor::new(buffer, noise))
-        }
-        (ReturnValue::Constant(val), ReturnValue::Buffer(buffer))
-        | (ReturnValue::Buffer(buffer), ReturnValue::Constant(val)) => {
-            compiler.push_visitor(MulConstantVisitor::new(buffer, val))
-        }
-        (ReturnValue::Buffer(left), ReturnValue::Buffer(right)) => {
-            compiler.push_visitor(MulBufferVisitor::new(left, right))
-        }
-    }
 }
+
+compile_commutative!(compile_add, AddBufferVisitor, AddConstantVisitor, AddNoiseVisitor);
+compile_commutative!(compile_mul, MulBufferVisitor, MulConstantVisitor, MulNoiseVisitor);
+compile_commutative!(compile_min, MinBufferVisitor, MinConstantVisitor, MinNoiseVisitor);
+compile_commutative!(compile_max, MaxBufferVisitor, MaxConstantVisitor, MaxNoiseVisitor);
+compile_non_commutative!(compile_sub, SubBufferVisitor, SubConstantVisitor, SubNoiseVisitor);
+compile_non_commutative!(compile_div, DivBufferVisitor, DivConstantVisitor, DivNoiseVisitor);
--- src/world/density/src/cpu/compiler/visitor/math.rs
+++ src/world/density/src/cpu/compiler/visitor/math.rs
@@ -1,9 +1,7 @@
 use crate::cpu::compiler::AnyBufferId;
 use crate::cpu::noise::NoiseAccessor;
-use crate::cpu::runtime::{
-    BufferAdd, BufferMul, ConstantAdd, ConstantMul, NoiseAdd, NoiseMul, YClampedGradient,
-};
-use crate::{impl_commutative_visitor, impl_direct_visitor, impl_visitor_base};
+use crate::cpu::runtime::{BufferAdd, BufferDiv, BufferMax, BufferMin, BufferMul, BufferSub, ConstantAdd, ConstantDiv, ConstantMax, ConstantMin, ConstantMul, ConstantSub, NoiseAdd, NoiseDiv, NoiseMax, NoiseMin, NoiseMul, NoiseSub, YClampedGradient};
+use crate::{impl_commutative_visitor, impl_direct_visitor, impl_non_commutative_visitor, impl_visitor_base};
 use std::ops::RangeInclusive;
 
 impl_visitor_base!(AddBufferVisitor, other: AnyBufferId);
@@ -14,21 +12,21 @@ impl_visitor_base!(MulBufferVisitor, other: AnyBufferId);
 impl_visitor_base!(MulConstantVisitor, other: f32);
 impl_visitor_base!(MulNoiseVisitor, other: NoiseAccessor);
 
-// impl_visitor_base!(MinBufferVisitor, other: AnyBufferId);
-// impl_visitor_base!(MinConstantVisitor, other: f32);
-// impl_visitor_base!(MinNoiseVisitor, other: NoiseAccessor);
-//
-// impl_visitor_base!(MaxBufferVisitor, other: AnyBufferId);
-// impl_visitor_base!(MaxConstantVisitor, other: f32);
-// impl_visitor_base!(MaxNoiseVisitor, other: NoiseAccessor);
-//
-// impl_visitor_base!(SubBufferVisitor, other: AnyBufferId);
-// impl_visitor_base!(SubConstantVisitor, other: f32);
-// impl_visitor_base!(SubNoiseVisitor, other: NoiseAccessor);
-//
-// impl_visitor_base!(DivBufferVisitor, other: AnyBufferId);
-// impl_visitor_base!(DivConstantVisitor, other: f32);
-// impl_visitor_base!(DivNoiseVisitor, other: NoiseAccessor);
+impl_visitor_base!(MinBufferVisitor, other: AnyBufferId);
+impl_visitor_base!(MinConstantVisitor, other: f32);
+impl_visitor_base!(MinNoiseVisitor, other: NoiseAccessor);
+
+impl_visitor_base!(MaxBufferVisitor, other: AnyBufferId);
+impl_visitor_base!(MaxConstantVisitor, other: f32);
+impl_visitor_base!(MaxNoiseVisitor, other: NoiseAccessor);
+
+impl_visitor_base!(SubBufferVisitor, other: AnyBufferId);
+impl_visitor_base!(SubConstantVisitor, other: f32);
+impl_visitor_base!(SubNoiseVisitor, other: NoiseAccessor);
+
+impl_visitor_base!(DivBufferVisitor, other: AnyBufferId);
+impl_visitor_base!(DivConstantVisitor, other: f32);
+impl_visitor_base!(DivNoiseVisitor, other: NoiseAccessor);
 
 impl_visitor_base!(YClampedGradientVisitor, y_range: RangeInclusive<i16>, value_range: RangeInclusive<f32>);
 
@@ -40,7 +38,20 @@ impl_commutative_visitor!(MulBufferVisitor, BufferMul, other);
 impl_direct_visitor!(MulConstantVisitor, ConstantMul, dst, src: other);
 impl_direct_visitor!(MulNoiseVisitor, NoiseMul, dst, src: other);
 
-// impl_commutative_visitor!(MinBufferVisitor, BufferMin, other);
-// impl_commutative_visitor!(MaxBufferVisitor, BufferMax, other);
+impl_commutative_visitor!(MinBufferVisitor, BufferMin, other);
+impl_direct_visitor!(MinConstantVisitor, ConstantMin, dst, src: other);
+impl_direct_visitor!(MinNoiseVisitor, NoiseMin, dst, src: other);
+
+impl_commutative_visitor!(MaxBufferVisitor, BufferMax, other);
+impl_direct_visitor!(MaxConstantVisitor, ConstantMax, dst, src: other);
+impl_direct_visitor!(MaxNoiseVisitor, NoiseMax, dst, src: other);
+
+impl_non_commutative_visitor!(SubBufferVisitor, BufferSub, other);
+impl_direct_visitor!(SubConstantVisitor, ConstantSub, dst, src: other);
+impl_direct_visitor!(SubNoiseVisitor, NoiseSub, dst, src: other);
+
+impl_non_commutative_visitor!(DivBufferVisitor, BufferDiv, other);
+impl_direct_visitor!(DivConstantVisitor, ConstantDiv, dst, src: other);
+impl_direct_visitor!(DivNoiseVisitor, NoiseDiv, dst, src: other);
 
 impl_direct_visitor!(YClampedGradientVisitor, YClampedGradient, dst, y_range: y_range, value_range: value_range);
