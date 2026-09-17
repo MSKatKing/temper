cb11ad803d33f9171f382f635d5b843ce4f4f5ba
MSKatKing <mskatking08@gmail.com>
2026-09-17 10:54:07 -0400

    Cleanup some code

--- src/world/noise/src/maps/blended.rs
+++ src/world/noise/src/maps/blended.rs
@@ -9,10 +9,8 @@ pub struct BlendedNoise {
     min_limit_noise: PerlinNoise,
     max_limit_noise: PerlinNoise,
     main_noise: PerlinNoise,
-    xz_multiplier: f64,
-    y_multiplier: f64,
-    xz_factor: f64,
-    y_factor: f64,
+    multiplier: DVec3,
+    factor: DVec3,
     smear_scale_multiplier: f64,
 }
 
@@ -54,27 +52,18 @@ impl BlendedNoise {
                 &(-15..=0).into_iter().collect::<Vec<_>>(),
             ),
             main_noise: PerlinNoise::new_legacy(rand, &(-7..=0).into_iter().collect::<Vec<_>>()),
-            xz_factor,
-            y_factor,
+            multiplier: 684.412 * DVec3::new(xz_scale, y_scale, xz_scale),
+            factor: DVec3::new(xz_factor, y_factor, xz_factor),
             smear_scale_multiplier,
-            xz_multiplier: 684.412 * xz_scale,
-            y_multiplier: 684.412 * y_scale,
         }
     }
 
     pub fn noise(&self, pos: DVec3) -> f64 {
-        let limit_x = pos.x * self.xz_multiplier;
-        let limit_y = pos.y * self.y_multiplier;
-        let limit_z = pos.z * self.xz_multiplier;
-        let limit = DVec3::new(limit_x, limit_y, limit_z);
+        let limit = pos * self.multiplier;
+        let main = limit / self.factor;
 
-        let main_x = limit_x / self.xz_factor;
-        let main_y = limit_y / self.y_factor;
-        let main_z = limit_z / self.xz_factor;
-        let main = DVec3::new(main_x, main_y, main_z);
-
-        let limit_smear = self.y_multiplier * self.smear_scale_multiplier;
-        let main_smear = limit_smear / self.y_factor;
+        let limit_smear = self.multiplier.y * self.smear_scale_multiplier;
+        let main_smear = limit_smear / self.factor.y;
         let mut blend_min = 0.0;
         let mut blend_max = 0.0;
         let mut main_noise_value = 0.0;
@@ -85,7 +74,7 @@ impl BlendedNoise {
             main_noise_value += noise.noise_advanced(
                 main.map(|v| PerlinNoise::wrap(v * pow)),
                 main_smear * pow,
-                main_y * pow,
+                main.y * pow,
             ) / pow;
 
             pow /= 2.0;
@@ -101,11 +90,11 @@ impl BlendedNoise {
             let y_scale_pow = limit_smear * pow;
 
             if !is_max && let (noise, _) = self.min_limit_noise.get_octave_noise(i) {
-                blend_min += noise.noise_advanced(w, y_scale_pow, limit_y * pow) / pow;
+                blend_min += noise.noise_advanced(w, y_scale_pow, limit.y * pow) / pow;
             }
 
             if !is_min && let (noise, _) = self.max_limit_noise.get_octave_noise(i) {
-                blend_max += noise.noise_advanced(w, y_scale_pow, limit_y * pow) / pow;
+                blend_max += noise.noise_advanced(w, y_scale_pow, limit.y * pow) / pow;
             }
 
             pow /= 2.0;
